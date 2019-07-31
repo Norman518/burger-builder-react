c382c646e6d8288ca00ba371039af6a6d38b22a2
Vincent Teas <vincentteas@gmail.com>
2019-07-31 16:34:12 -0700

    able to add ingredients
    able to add ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,12 @@ import React, { Component } from "react";
 import Auxillary from "../../hoc/Auxillary";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
-
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.65,
+  meat: 1,
+  bacon: 0.75
+};
 class BurgerBuilder extends Component {
   // constructor(props) {
   //     super(props);
@@ -15,14 +20,27 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 2
   };
+
+  addIngredientHandler = type => {
+    const updatedCount = this.state.ingredients[type] + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  removeIngredientHandler = type => {};
 
   render() {
     return (
       <Auxillary>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </Auxillary>
     );
   }
