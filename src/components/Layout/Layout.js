910e6eecd6108f10da0ed9303c35a82fa2a5d08a
Vincent Teas <vincentteas@gmail.com>
2019-08-01 11:32:18 -0700

    added sidebar style
    added sidebar style

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Auxillary from "../../hoc/Auxillary";
 import classes from "./Layout.css";
-import Toolbar from '../Navigation/Toolbar/Toolbar'
-
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 const layout = props => (
   <Auxillary>
-    <Toolbar/>
+    <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Auxillary>
 );
