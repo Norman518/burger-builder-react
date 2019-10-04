0353d6d79ec9b13df29d4ac8047661bb23340128
Vincent Teas <46739285+Norman518@users.noreply.github.com>
2019-10-04 11:21:25 -0400

    Update ContactData.js

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,7 +84,7 @@ class ContactData extends Component {
                         { value: 'cheapest', displayValue: 'Cheapest' }
                     ]
                 },
-                value: '', valid: true
+                value: 'fastest', validation: {}, valid: true
             }
         },
         formIsValid: false,
@@ -114,6 +114,9 @@ class ContactData extends Component {
     }
     checkValidity(value, rules) {
         let isValid = true;
+        if (!rules) {
+            return true;
+        }
         if (rules.required) {
             isValid = value.trim() !== '' && isValid;
         }
